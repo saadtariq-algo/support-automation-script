04c67cf3ef833f30b70e05f9a3c5815d8c6f59ea
saadtariq-algo <74005199+saadtariq-algo@users.noreply.github.com>
2021-03-21 04:09:08 +0500

    Add files via upload

---
+++ support-automation-script/config.py
@@ -0,0 +1,14 @@
+# <-- DataBase And Slack Connectivity  --> #
+
+uri = "mongodb://saad-tariq:lrf8A96D6KxYz5q1iItdRda1boVFPh@algomus-prod-shard-00-00-yjdp2.mongodb.net:27017,algomus-prod-shard-00-01-yjdp2.mongodb.net:27017,algomus-prod-shard-00-02-yjdp2.mongodb.net:27017/algomus_prod?authSource=admin&replicaSet=Algomus-PROD-shard-0&retryWrites=true&ssl=true"
+
+#<-- Algo workspace Support_automate channel --> #
+
+webhook_url='https://hooks.slack.com/services/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'
+
+
+
+
+
+
+
